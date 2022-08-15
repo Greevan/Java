13885ff7bf2dfc70b0444e22c2cc5734fe615ce3
Dhanush <mrblue@localhost.localdomain>
2022-08-15 20:48:52 +0530

    Re-edited the Fibonacci

--- Javapoint/JavaBasics/fibonacci.java
+++ Javapoint/JavaBasics/fibonacci.java
@@ -1,12 +1,13 @@
-class fibonacci{
-    public static void main(String[] args) {
-      int m=0,n=1,o,i,count=10;
-      System.out.print(m+" "+n);
-      for(i=2;i<count;i++){
-          o=m+n;
-          System.out.print(" "+o);
-          m=n;
-          n=o;
-      }
-    }
+public class fibonacci {
+	public static void main(String[] args){
+		int n1=0,n2=1,n3,i,count=10;
+		System.out.print(n1+" "+n2);
+		
+		for(i=2;i<count;++i){
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+	}
 }
